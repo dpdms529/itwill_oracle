0e305b432d3ae3f2f59567740599749bff10159f
dpdms529 <musiclove529@naver.com>
2025-10-31 18:05:49 +0900

    251031

--- section251031/dba.sql
+++ section251031/dba.sql
@@ -1,10 +1,12 @@
 SELECT * FROM dba_tables WHERE table_name = 'EMP';
-SELECT * FROM dba_segments WHERE segment_name = 'EMP';
-SELECT * FROM dba_extents WHERE segment_name = 'EMP';
+SELECT * FROM dba_segments WHERE segment_name = 'EMP' AND owner = 'HR';
+SELECT * FROM dba_extents WHERE segment_name = 'EMP' AND owner = 'HR';
 
 SELECT * FROM dba_tablespaces;
 SELECT * FROM dba_data_files;
 SELECT * FROM dba_temp_files;
+
+SELECT * FROM dba_recyclebin;
 
 -- comment
 -- 테이블 주석 확인
